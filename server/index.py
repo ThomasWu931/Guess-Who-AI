93fc6cff441e0cc1c1ff4685ab7754340a9fa873
Thomas Wu <thomaswu@getathelas.com>
2024-09-18 17:51:25 -0400

    shift app and requirements

--- server/index.py
+++ server/index.py
@@ -1,10 +1,10 @@
-# from flask import Flask, jsonify, request
-# from GWB.ml_model.model import *
-# app = Flask(__name__)
+from flask import Flask, jsonify, request
+from ..ml_model.model import *
+app = Flask(__name__)
 
-# incomes = [
-#     { 'description': 'salary', 'amount': 5000 }
-# ]
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
 # model = MLModel(img_h=218,img_w=178,weights_dir="ml_model/weights",feature="Eyeglasses",load_weights=True)
 # @app.route('/incomes')
 # def get_incomes():
@@ -12,12 +12,12 @@
 #     full_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 #     return model.predict(full_paths).tolist()
 
-# @app.route('/')
-# def hello_world():
-#     return "hello-world"
+@app.route('/')
+def hello_world():
+    return "hello-world"
 
-# # @app.route('/incomes', methods=['POST'])
-# # def add_income():
-# #     breakpoint()
-# #     incomes.append(request.get_json())
-# #     return '', 204
+# @app.route('/incomes', methods=['POST'])
+# def add_income():
+#     breakpoint()
+#     incomes.append(request.get_json())
+#     return '', 204
