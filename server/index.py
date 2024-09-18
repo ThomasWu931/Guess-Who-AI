28f7f69ab5d55ef1019683ba391002929086b4a9
Thomas Wu <thomas.wu@spire.com>
2024-09-18 16:30:46 -0400

    undo comments + add absolute import

--- server/index.py
+++ server/index.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify, request
-# from ..ml_model.model import *
+from GWB.ml_model.model import *
 app = Flask(__name__)
 
-# incomes = [
-#     { 'description': 'salary', 'amount': 5000 }
-# ]
-# model = MLModel(img_h=218,img_w=178,weights_dir="ml_model/weights",feature="Eyeglasses",load_weights=True)
-# @app.route('/incomes')
-# def get_incomes():
-#     directory = "./ml_model/data/Test"
-#     full_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-#     return model.predict(full_paths).tolist()
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+model = MLModel(img_h=218,img_w=178,weights_dir="ml_model/weights",feature="Eyeglasses",load_weights=True)
+@app.route('/incomes')
+def get_incomes():
+    directory = "./ml_model/data/Test"
+    full_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    return model.predict(full_paths).tolist()
 
 @app.route('/')
 def hello_world():
