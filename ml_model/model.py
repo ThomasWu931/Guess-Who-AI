a8c02d335c2ccb36e53f095cb8104c868132207e
Thomas Wu <thomas.wu@spire.com>
2024-09-19 16:01:48 -0400

    import from model instead of weights + edit api to make sample predict call

--- ml_model/model.py
+++ ml_model/model.py
@@ -13,39 +13,35 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 
-
 class MLModel:
-    def __init__(self, img_h, img_w, model_path, load_weights=False):
+    def __init__(self, img_h, img_w, model_path):
         self.img_h = img_h
         self.img_w = img_w
-        self.weights_dir = weights_dir
-        self.feature = feature
-        self.model = self.initialize_model()
-        if load_weights:
-            self.load_weights()
+        self.model_path = model_path
+        self.model = keras.saving.load_model(model_path)
 
-    def get_weights_path(self):
-        return f"{self.weights_dir}/feature-{self.feature}.keras"
+    # def get_weights_path(self):
+    #     return f"{self.weights_dir}/feature-{self.feature}.keras"
 
-    def load_weights(self) -> None:
-        weights_path = self.get_weights_path()
-        self.model.load_weights(weights_path)
+    # def load_weights(self) -> None:
+    #     weights_path = self.get_weights_path()
+    #     self.model.load_weights(weights_path)
 
-    def initialize_model(self, model="VGG16"):
-        if model == "VGG16":
-            base_model = keras.applications.VGG16(weights=None, input_shape=(self.img_h, self.img_w, 3), include_top=False)
-        elif model == "ResNet50V2":
-            base_model = keras.applications.ResNet50V2(weights=None, input_shape=(self.img_h, self.img_w, 3), include_top=False)
-        else:
-            raise Exception(f"[initialize_model] ERROR: Invalid model type {model}")
-        add_model = Sequential()
-        add_model.add(Flatten(input_shape=base_model.output_shape[1:]))
-        add_model.add(Dense(1, activation='sigmoid'))
-        model = Model(inputs=base_model.input, outputs=add_model(base_model.output))
-        model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0001),
-                        metrics=['accuracy'])
-        model.summary()
-        return model
+    # def initialize_model(self, model="VGG16"):
+    #     if model == "VGG16":
+    #         base_model = keras.applications.VGG16(weights=None, input_shape=(self.img_h, self.img_w, 3), include_top=False)
+    #     elif model == "ResNet50V2":
+    #         base_model = keras.applications.ResNet50V2(weights=None, input_shape=(self.img_h, self.img_w, 3), include_top=False)
+    #     else:
+    #         raise Exception(f"[initialize_model] ERROR: Invalid model type {model}")
+    #     add_model = Sequential()
+    #     add_model.add(Flatten(input_shape=base_model.output_shape[1:]))
+    #     add_model.add(Dense(1, activation='sigmoid'))
+    #     model = Model(inputs=base_model.input, outputs=add_model(base_model.output))
+    #     model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0001),
+    #                     metrics=['accuracy'])
+    #     model.summary()
+    #     return model
 
     def predict(self, image_paths: list[str], show_visual=False) -> np.ndarray:
         images = []
--- server/index.py
+++ server/index.py
@@ -1,25 +1,22 @@
 from flask import Flask, jsonify, request
-# from ..ml_model.model import *
+from ml_model.model import *
+
 app = Flask(__name__)
 
 incomes = [
     { 'description': 'salary', 'amount': 5000 }
 ]
-# model = MLModel(img_h=218,img_w=178,weights_dir="ml_model/weights",feature="Eyeglasses",load_weights=True)
-# @app.route('/incomes')
-# def get_incomes():
-#     directory = "./ml_model/data/Test"
-#     full_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-#     return model.predict(full_paths).tolist()
+# model = MLModel(img_h=218,img_w=178,weights_dir="./ml_model/weights",feature="Eyeglasses",load_weights=True)
+# model.model.save("Eyeglasses.keras")
+model = MLModel(img_h=218,img_w=178, model_path="ml_model/models/Eyeglasses.keras")
+@app.route('/incomes')
+def get_incomes():
+    directory = "./ml_model/data/Test"
+    full_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    return model.predict(full_paths).tolist()
 
 @app.route('/')
 def hello_world():
     return "hello-world"
-
-# @app.route('/incomes', methods=['POST'])
-# def add_income():
-#     breakpoint()
-#     incomes.append(request.get_json())
-#     return '', 204
 
 app.run(host='0.0.0.0', port=5000)
