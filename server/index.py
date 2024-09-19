63b822b636a731171e6f0bbb9ec6d84cd2672ee6
Thomas Wu <thomas.wu@spire.com>
2024-09-19 16:15:49 -0400

    comment out

--- server/index.py
+++ server/index.py
@@ -2,18 +2,15 @@ from flask import Flask, jsonify, request
 from ml_model.model import *
 
 app = Flask(__name__)
-
-incomes = [
-    { 'description': 'salary', 'amount': 5000 }
-]
+W
 # model = MLModel(img_h=218,img_w=178,weights_dir="./ml_model/weights",feature="Eyeglasses",load_weights=True)
 # model.model.save("Eyeglasses.keras")
 model = MLModel(img_h=218,img_w=178, model_path="ml_model/models/Eyeglasses.keras")
-@app.route('/incomes')
-def get_incomes():
-    directory = "./ml_model/data/Test"
-    full_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    return model.predict(full_paths).tolist()
+# @app.route('/incomes')
+# def get_incomes():
+#     directory = "./ml_model/data/Test"
+#     full_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+#     return model.predict(full_paths).tolist()
 
 @app.route('/')
 def hello_world():
